79f7744c529abb2a9b315395f8090f773987d734
Koak DoHun <dori20051@gmail.com>
2024-02-19 23:55:02 +0900

    upload

---
+++ src/db/mysql.js
@@ -0,0 +1,47 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user: 'nodetest',
+    password: 'test1234',
+    port: 3306,
+    database:'NODE_DB'
+});
+
+function getAllTest(callback){
+    connection.query(`SELECT * FROM TODO`, (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function postTest(callback, time, data) {
+    connection.query(`INSERT INTO todo (time, text, work, complete) VALUES ('${time}', '${data["text"] }', ${data["working"]==true?1:0}, ${data["complete"]==true?1:0})`, (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+function patchTest(callback, key, text, complete) {
+    if (text) {
+        connection.query(`UPDATE todo SET text = '${text}', complete = '${complete?1:0}' WHERE time = '${key}'`, (err, rows, fields) => { 
+            callback(rows);
+        });
+    } else {
+        connection.query(`UPDATE todo SET complete = '${complete?1:0}' WHERE time = '${key}'`, (err, rows, fields) => { 
+            callback(rows);
+        });
+    }
+}
+
+function deleteTest(callback, key) {
+    connection.query(`DELETE FROM todo WHERE time = '${key}'`, (err, rows, fields) => {
+        callback(rows);
+    });
+}
+
+module.exports = {
+    getAllTest,
+    postTest,
+    patchTest,
+    deleteTest,
+}
---
+++ src/server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const db = require('./db/mysql');
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:19006');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.get('/', (req, res) => {
+  db.getAllTest((rows) => {
+    console.log(rows);
+    res.json(rows);
+  });
+})
+
+app.post('/test2', (req, res) => {
+  console.log("call post");
+  Object.keys(req.body).forEach((time)=> {
+    db.postTest((rows) => {
+      res.statusCode = 200;
+    }, time, req.body[time]);
+  });
+  res.send('test pass');
+})
+
+app.patch('/test2', (req, res) => {
+  console.log("call patch");
+  Object.keys(req.body).forEach((time)=> {
+    db.patchTest((rows) => {
+      console.log(rows);
+      res.statusCode = 200;
+    }, time, req.body[time].text, req.body[time].complete);
+  });
+  res.send('test pass');
+});
+
+app.delete('/test2', (req, res) => {
+  console.log(req.body);
+  Object.keys(req.body).forEach((time)=> {
+    db.deleteTest((rows) => {
+      console.log(rows);
+      res.statusCode = 200;
+    }, time);
+  });
+  res.send('test pass');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})

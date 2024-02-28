04a6a96b491ea9ddfa668eb48098e90f9801a09c
Koak DoHun <dori20051@gmail.com>
2024-02-28 23:45:43 +0900

    server update

--- LoginServer/src/db/mysql.js
+++ LoginServer/src/db/mysql.js
@@ -9,15 +9,33 @@ const connection = mysql.createConnection({
 });
 
 function postLoginTest(callback, id) {
-    connection.query(`SELETE pw FROM login WHERE id = '${id}`, (err, rows, fields) => {
+    connection.query(`SELECT pw FROM login WHERE id='${id}'`, (err, rows, fields) => {
         if(err) throw err;
         callback(rows);
     });
 }
+
+function enrollCertification(callback, id, cert) {
+    connection.query(`INSERT INTO cert (id) VALUES ('${cert}')`, (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function signUpTest(callback, id, pw) {
+    connection.query(`INSERT INTO login (id, pw) VALUES ('${id}', '${pw}')`, (err, rows, fields)=> {
+        if(err) throw err;
+        callback(rows);
+    })
+}
+function reSignTest(callback, id) {
+    connection.query(`DELETE FROM login WHERE id = '${id}'`, (err, rows, fields)=> {
+        if(err) throw err;
+        callback(rows);
+    })
+}
 module.exports = {
-    getAllTest,
-    postTest,
-    patchTest,
-    deleteTest,
     postLoginTest,
+    enrollCertification,
+    signUpTest
 }
--- LoginServer/src/server.js
+++ LoginServer/src/server.js
@@ -1,36 +1,64 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
 const db = require('./db/mysql');
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:19006');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
-app.post('/login-token', (req, res) => {
-  console.log("call post");
-  var data = {}
+app.post('/login-token', async(req, res) => {
+  console.log(req.body);
+  var test = false;
+  const data = {
+    id : req.body.id,
+    cert : "test"
+  }
   db.postLoginTest((rows) => {
     if (rows === req.body.pw) {
-      data = {
-        id : req.body.id,
-        cert : "test"
-      };
       res.statusCode = 200;
     }else 
       res.statusCode = 400;
   }, req.body.id);
-  db.enrollCertification((rows)=>{
-    console.log("enroll success!");
-  },data)
-  res.send(data);
+
+  console.log(test);
+  // db.enrollCertification((rows)=>{
+  //   console.log("enroll success!");
+  // }, req.body.id, data.cert)
+  
+  return res.send(JSON.stringify(data));
 })
+
+app.post('/user', (req, res) => {
+  console.log(req.body);
+  db.signUpTest((rows) => {
+    console.log(rows);
+    res.status(200).send(rows);
+  }, req.body.id, req.body.pw);
+})
+app.get('/user/:id', (req, res) => {
+  db.postLoginTest((rows) => {
+    if (rows === req.query.pw) {
+      res.statusCode = 200;
+    }else 
+      res.statusCode = 400;
+  }, req.params.id);
+})
+app.delete('/user/:id', (req, res) => {
+  db.postLoginTest((rows) => {
+    if (rows === req.query.pw) {
+      res.statusCode = 200;
+    }else 
+      res.statusCode = 400;
+  }, req.params.id);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
